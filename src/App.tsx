9a678f2d3f313e1b281d6e9c52b7ff9085c6e1ac
Asif Quadir <asif.anik2000@gmail.com>
2023-08-01 09:44:41 +0600

    text color changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <TodosProvider>
-      <main className="min-h-screen w-screen bg-neutral-800 text-neutral-400 flex flex-col justify-start items-center py-10">
+      <main className="min-h-screen w-screen bg-neutral-800 text-neutral-600 flex flex-col justify-start items-center py-10">
         <h1 className="text-xl">TODO REACT + TYPESCRIPT</h1>
         <Navbar />
         <Outlet />
